237613063adff7aa4ea9ce8d0619f639a4bb6781
Endeavour-1 <50341461+Endeavour-Innovations-Inc@users.noreply.github.com>
2021-10-27 15:40:36 -0700

    Added my commit files

--- app/Model/models.py
+++ app/Model/models.py
@@ -3,6 +3,7 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, generate_password_hash, check_password_hash
  
+# Al commits
 ## changing login.html, register.html, models.py, auth_routes.py, auth_forms.py
 
 postTags = db.Table('PostTag',
